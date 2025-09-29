efaddd08b9e2fe0ecc69210e11dd6f95b69605cc
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-09-29 23:55:46 +0600

    Student Model Added

---
+++ EngWing Jet Server/src/app/modules/student/student.model.ts
@@ -0,0 +1,75 @@
+import { model, Schema } from 'mongoose'
+import { TName, TStudent } from './student.interface'
+
+const nameSchema = new Schema<TName>({
+  firstName: {
+    type: String,
+    required: [true, 'First Name is required'],
+    trim: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'First Name is required'],
+    trim: true,
+  },
+})
+
+const studentSchema = new Schema<TStudent>(
+  {
+    id: {
+      type: String,
+      required: [true, 'Id is required'],
+      unique: true,
+    },
+    name: {
+      type: nameSchema,
+      required: [true, 'Name is required'],
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ['male', 'female', 'other'],
+        message: 'Gender can be male | female | other',
+      },
+      required: true,
+    },
+    dateOfBirth: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    contactNo: {
+      type: String,
+      required: true,
+    },
+    emergencyContactNo: {
+      type: String,
+      required: true,
+    },
+    presentAddress: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+    },
+    academicLevel: {
+      type: String,
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true },
+)
+
+export const Student = model<TStudent>('Student', studentSchema)
