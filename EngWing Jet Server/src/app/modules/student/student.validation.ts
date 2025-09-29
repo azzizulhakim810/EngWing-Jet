3ce3ca506d96e2949436e413d9b6f1b86323832f
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-09-30 01:16:24 +0600

    Student validation Added

---
+++ EngWing Jet Server/src/app/modules/student/student.validation.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod'
+
+export const nameValidationSchema = z.object({
+  firstName: z
+    .string()
+    .max(10, 'First Name cant be more than 10')
+    .trim()
+    .nonempty('First Name is required'),
+  middleName: z.string().optional(),
+  lastName: z
+    .string()
+    .min(1)
+    .trim()
+    .nonempty('Last Name is required')
+    .regex(/^[A-Za-z]+$/, { message: 'Last Name must contain only letters' }),
+})
+
+export const studentValidationSchema = z.object({
+  id: z.string().nonempty('Student ID is required'),
+
+  name: nameValidationSchema,
+
+  gender: z.enum(['male', 'female', 'other'], {
+    error: 'Gender can be male | female | other',
+  }),
+
+  dateOfBirth: z.string().optional(),
+
+  email: z.email('Invalid email format').nonempty('Email is required'),
+
+  contactNo: z.string().nonempty('Contact Number is required'),
+
+  emergencyContactNo: z
+    .string()
+    .nonempty('Emergency Contact Number is required'),
+
+  presentAddress: z.string().nonempty('Present Address is required'),
+
+  profileImage: z.string().optional(),
+
+  academicLevel: z.string().nonempty('Please specify your Academic Level'),
+
+  isDeleted: z.boolean().default(false),
+})
+
+export default studentValidationSchema
